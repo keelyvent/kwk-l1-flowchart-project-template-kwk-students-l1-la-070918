113c2b3b13590ee0f92c18243d341ad61dcef36b
keelyvent <kvent11@yahoo.com>
2018-07-20 19:20:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
-class ApplicationController < Sinatra::Base
+# require_relative 'config/environment'
 
-  configure do
+class ApplicationController < Sinatra::Base
+configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  get '/' do
+    erb :index
+  end
+  # Add your post route and action below
+post '/' do 
+  #the first part we can make up = the second part is already assigned names in index 
+  @the_result =params[:user_result]
+  
+  return erb :results
 end
+ 
+end
+

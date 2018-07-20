4c3a1db4c64f413714779a378689e96e6ecb7e93
keelyvent <kvent11@yahoo.com>
2018-07-20 20:14:40 +0000

    stand up to the stigma woot woot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
-class ApplicationController < Sinatra::Base
+# require_relative 'config/environment'
 
-  configure do
+class ApplicationController < Sinatra::Base
+configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  get '/' do
+    erb :index
+  end
+  
+  get '/index' do 
+    erb :index
+  end 
+  
+  # Add your post route and action below
+post '/' do 
+  #the first part we can make up = the second part is already assigned names in index 
+  @the_result =res(params[:good_day],params[:bip],params[:anx],params[:ed],params[:dep])
+  
+  
+  return erb :results
 end
+ 
+end
+
